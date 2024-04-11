f2c8639ce1fdfec1156925a6aeb0afc8adec0105
Shashwot KC <49743516+shashwot2@users.noreply.github.com>
2024-04-11 14:32:25 -0400

    Add serial version matrix multiplication

---
+++ main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void matrixMultiply(int **A, int **B, int **C, int N) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            C[i][j] = 0;
+            for (int k = 0; k < N; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Usage: %s <matrix size>\n", argv[0]);
+        return 1;
+    }
+    int N = atoi(argv[1]);
+    int **A, **B, **C;
+
+    A = (int **)malloc(N * sizeof(int *));
+    B = (int **)malloc(N * sizeof(int *));
+    C = (int **)malloc(N * sizeof(int *));
+    for (int i = 0; i < N; i++) {
+        A[i] = (int *)malloc(N * sizeof(int));
+        B[i] = (int *)malloc(N * sizeof(int));
+        C[i] = (int *)malloc(N * sizeof(int));
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            A[i][j] = (i == j) ? 1 : 0;
+            B[i][j] = 1;
+        }
+    }
+
+    matrixMultiply(A, B, C, N);
+
+    printf("Matrix C (Result):\n");
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%d ", C[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < N; i++) {
+        free(A[i]);
+        free(B[i]);
+        free(C[i]);
+    }
+    free(A);
+    free(B);
+    free(C);
+
+    return 0;
+}
