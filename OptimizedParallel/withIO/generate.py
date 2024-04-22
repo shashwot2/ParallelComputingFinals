544567e1d8844359f176b32f84195e967715e237
Shashwot KC <49743516+shashwot2@users.noreply.github.com>
2024-04-22 11:29:14 -0400

    Add slight modification

--- OptimizedParallel/withIO/generate.py
+++ OptimizedParallel/withIO/generate.py
@@ -1,8 +1,16 @@
+import sys
 import numpy as np
 
-N = 4  
+if len(sys.argv) != 4:
+    print("Usage: python generate_matrices.py <matrix_size> <output_file_a> <output_file_b>")
+    sys.exit(1)
+
+N = int(sys.argv[1])
+output_file_a = sys.argv[2]
+output_file_b = sys.argv[3]
+
 a = np.random.randint(0, 10, size=(N, N))
 b = np.random.randint(0, 10, size=(N, N))
 
-a.astype('int32').tofile('matrix_a.dat')
-b.astype('int32').tofile('matrix_b.dat')
+a.astype('int32').tofile(output_file_a)
+b.astype('int32').tofile(output_file_b)
--- OptimizedParallel/withIO/highlifeMPI-CUDA.c
+++ OptimizedParallel/withIO/highlifeMPI-CUDA.c
@@ -2,23 +2,25 @@
 #include <stdlib.h>
 #include <mpi.h>
 
-int N= 4;
-void print_results(char *prompt, int *a, int rows, int cols);
-extern void matrixMulCUDA(int *a, int *b, int *c, int width, int local_width, int threads);
+
+void print_results(char *prompt, int* a, int rows, int cols);
+extern void matrixMulCUDA(int *a, int* b, int *c, int width, int local_width, int threads);
 
 int main(int argc, char *argv[])
 {
-    // IMPORTANT PLEASE READ: The matrix size is hardcoded here
-    // Matrix size actually shouldn't be defined here in input withIO because the size works from reading the input file directly
     int rank, size;
+
     MPI_Init(&argc, &argv);
-    if (argc != 2)
+
+    if (argc != 3)
     {
         fprintf(stderr, "Usage: %s <matrixsize> <threads>\n", argv[0]);
         MPI_Abort(MPI_COMM_WORLD, 1);
     }
-    N = atoi(argv[1]);
+
+    int N = atoi(argv[1]);
     int threads = atoi(argv[2]);
+
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
@@ -34,7 +36,7 @@ int main(int argc, char *argv[])
         displs[i] = i * rows_per_process * N;
         if (i == size - 1)
         {
-            send_counts[i] += remaining_rows * N; 
+            send_counts[i] += remaining_rows * N;
         }
     }
 
@@ -48,7 +50,7 @@ int main(int argc, char *argv[])
     // Read matrix A
     if (MPI_File_open(MPI_COMM_WORLD, "matrix_a.dat", MPI_MODE_RDONLY, MPI_INFO_NULL, &fh_a) != MPI_SUCCESS)
     {
-        fprintf(stderr, "Failed to open file 'matrix_a.txt'\n");
+        fprintf(stderr, "Failed to open file 'matrix_a.dat'\n");
         MPI_Abort(MPI_COMM_WORLD, 1);
     }
     MPI_File_read_at_all(fh_a, displs[rank] * sizeof(int), aa, send_counts[rank], MPI_INT, &status);
@@ -57,22 +59,23 @@ int main(int argc, char *argv[])
     // Read matrix B
     if (MPI_File_open(MPI_COMM_WORLD, "matrix_b.dat", MPI_MODE_RDONLY, MPI_INFO_NULL, &fh_b) != MPI_SUCCESS)
     {
-        fprintf(stderr, "Failed to open file 'matrix_b.txt'\n");
+        fprintf(stderr, "Failed to open file 'matrix_b.dat'\n");
         MPI_Abort(MPI_COMM_WORLD, 1);
     }
     MPI_File_read_at_all(fh_b, 0, bb, N * N, MPI_INT, &status);
     MPI_File_close(&fh_b);
 
     // Process local matrix multiplication
-    matrixMulCUDA(aa, bb, cc, N, send_counts[rank] / N);
+    matrixMulCUDA(aa, bb, cc, N, send_counts[rank] / N, threads);
 
     // Printing results
     if (N <= 12)
     {
-    print_results(prompt, cc, send_counts[rank] / N, N);
-    char prompt[100];
-    sprintf(prompt, "Result Matrix on Process %d:", rank);
+        char prompt[100];
+        sprintf(prompt, "Result Matrix on Process %d:", rank);
+        print_results(prompt, cc, send_counts[rank] / N, N);
     }
+
     MPI_File_open(MPI_COMM_WORLD, "result_matrix.dat", MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &fh_c);
     MPI_File_write_at_all(fh_c, displs[rank] * sizeof(int), cc, send_counts[rank], MPI_INT, MPI_STATUS_IGNORE);
     MPI_File_close(&fh_c);
@@ -88,7 +91,7 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-void print_results(char *prompt, int *a, int rows, int cols)
+void print_results(char *prompt, int* a, int rows, int cols)
 {
     printf("%s\n", prompt);
     for (int i = 0; i < rows; i++)
